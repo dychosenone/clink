f479c6321f7f40199f99d3a5368bab3521e60a8e
JulCL123 <77001355+JulCL123@users.noreply.github.com>
2021-08-18 20:58:46 +0800

    Update user.js

--- service/user.js
+++ service/user.js
@@ -14,7 +14,40 @@ const UserService = {
         }); 
 
         return newUser.save();
-    }
+    },
+    
+    updateUser: async (data) => {
+        try {
+            //const user = await UserService.getUser({_id: data.session.userId});
+            const user = await UserService.getUser({username: data.body.username});
+            console.log(user);
+            const { username, fullname, email, birthday, password, confirmpassword } = data.body; 
+            const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS); 
+
+            console.log(data.body.password);
+            console.log(data.body.confirmpassword);
+
+            if(fullname !== '') {
+                user.fullname = fullname; 
+            }
+        
+            if(email !== '') {
+                user.email = email; 
+            }
+        
+            if(birthday !== '') {
+                user.birthday = birthday; 
+            }
+        
+            if(password !== '') {
+                user.password = password; 
+            }
+            
+            return user.save();
+        } catch (err) {
+            throw err; 
+        }
+    }, 
 }
 
 
